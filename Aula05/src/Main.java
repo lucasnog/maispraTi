ca10cb16e0c4fe426ede9baf4b873665d165bd5e
Lucas Nogueira Lopes <lucasnog@msn.com>
2023-09-20 10:57:08 -0300

    feat: Aula05 - Tipos Wrappers e String

---
+++ Aula05/src/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        Integer x = 3400;
+        Integer y = 3400;
+
+      if (x.equals(y) ){
+          System.out.println("São iguais!");
+      }else{
+          System.out.println("Não são iguais!");
+      }
+
+
+        String frase = "Olá mundo!";
+        System.out.println(frase);
+        System.out.println(frase.length());
+        System.out.println(frase.toUpperCase());
+        System.out.println(frase.toLowerCase());
+
+
+    }
+}
